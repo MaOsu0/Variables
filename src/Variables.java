8ff0d9f3bc1177c13b05763abd0568d3cb74da23
MaOsu0 <mariusz.osuch1@gmail.com>
2022-02-08 19:45:46 +0100

    08.02.2022

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Variables {
+public class Variables {
+    public static void main(String[] args) {
+        String firstName;
+        firstName = "Jan";
+        var lastName = "Kowalski";
+        final String pesel = "1234567890";
+        int age = 25;
+        double height = 180;
+        double suma = height + 5;
+
+        System.out.println(firstName + " " + lastName + " ma " + age + " lat" );
+        System.out.println("Pan " + lastName + " ma pesel " + pesel);
+        System.out.println(firstName + " ma wzrost " + height + "cm");
+        System.out.println("Jeżeli " + firstName + " urośnie o 5cm, to będzie miał " + suma + "cm");
+
+        firstName = "Krzysztof";
+        System.out.println("Imię po zmianie "+ firstName);
+
+    }
 }
--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Zadanie1 {
+import javax.print.attribute.HashPrintServiceAttributeSet;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        String brand = "Seat";
+        String model = "Ibiza";
+        int productionDate = 2002;
+        double price = 20_000.55;
+
+        System.out.println("Sprzedam samochód marki "+ brand + " model " + model + " rok produkcji " + productionDate +
+                " za cenę " + price + ".");
+    }
 }
